51eec15e9668f846e6dc78688c0b40780b3d79ee
mamba001 <peter95zh@gmail.com>
2025-07-23 23:34:46 -0700

    day15: cross_val_score and day16: boosting

---
+++ leetcode/103_binary_tree_zigzag_level_order_traversal.py
@@ -0,0 +1,36 @@
+#from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        res = []
+        queue = deque([root])
+        left_to_right = True
+
+        while queue:
+            size = len(queue)
+            level = deque()
+            for _ in range(size):
+                node = queue.popleft()
+
+                if left_to_right:
+                    level.append(node.val)
+                else:
+                    level.appendleft(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            res.append(list(level))
+            left_to_right = not left_to_right
+
+        return res
